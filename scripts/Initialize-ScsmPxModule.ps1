5850da81ca54b0bd4c42b69424587cf15d107c72
Kirk Munro <KirkMunro@users.noreply.github.com>
2014-03-24 00:50:59 -0400

    added Test-Connection call to make for easier loading

--- scripts/Initialize-ScsmPxModule.ps1
+++ scripts/Initialize-ScsmPxModule.ps1
@@ -120,11 +120,15 @@ function Initialize-ScsmPxModule {
         }
         if ($defaultScsmManagementServer) {
             try {
+                # Ensure that we can ping the default SCSM Management Server before we try to connect to it.
+                if (-not (Test-Connection -ComputerName $defaultScsmManagementServer -Count 2 -ErrorAction SilentlyContinue)) {
+                    throw
+                }
                 New-SCManagementGroupConnection -ComputerName $defaultScsmManagementServer -ErrorAction Stop
             } catch {
                 # This will fail if the default management server is offline, in which case we
                 # absorb warn them about the error and allow the user to connect on their own.
-                Write-Warning "Unable to connect to the default SCSM Management Server ('${defaultScsmManagementServer}'). Use the New-SCManagementGroupConnection cmdlet to establish a valid connection to an SCSM Management Server."
+                Write-Warning "Unable to connect to the default SCSM Management Server ('${defaultScsmManagementServer}'). Use the New-SCManagementGroupConnection cmdlet to establish a valid connection to an SCSM Management Server that is online."
             }
         }
         if (-not (Get-Module -Name Microsoft.EnterpriseManagement.Warehouse.Cmdlets)) {
